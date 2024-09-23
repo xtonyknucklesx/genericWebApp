020d8dd8d9114106e3cc14a694c709236ea19ae7
xtonyknucklesx <anthony.rossi1983@gmail.com>
2024-09-23 10:14:50 -0700

    update

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,23 @@
 // backend/index.js
 
+require('dotenv').config();
+
+const dotenv = require('dotenv');
+
+if (process.env.NODE_ENV === 'production') {
+    dotenv.config({ path: './.env.prod'});
+} else if (process.env.NODE_ENV === 'development') {
+    dotenv.config({ path: './.env.dev'});
+} else if (process.env.NODE_ENV === 'test') {
+    dotenv.config( { path: './.env.test'});
+} else {
+    dotenv.config();
+}
+
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
+const dbUrl = process.env.DB_URL;
 
 app.get('/', (req, res) => {
     res.json({ message: 'Welcome to the Backend API of genericWebAPP'})
