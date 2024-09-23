58a3daa3dca52f2f4a3e5837beff45f1d1496808
xtonyknucklesx <anthony.rossi1983@gmail.com>
2024-09-23 11:08:19 -0700

    a

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,16 @@
 
 require('dotenv').config();
 
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 4000;
+const dbUrl = process.env.DB_URL;
+const jwtSecret = process.env.JWT_SECRET;
+const apiKey = process.env.API_KEY
+
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 
 if (process.env.NODE_ENV === 'production') {
     dotenv.config({ path: './.env.prod'});
@@ -14,15 +23,14 @@ if (process.env.NODE_ENV === 'production') {
     dotenv.config();
 }
 
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 4000;
-const dbUrl = process.env.DB_URL;
+app.use(morgan('combined')); // Logs requests to the console
 
 app.get('/', (req, res) => {
-    res.json({ message: 'Welcome to the Backend API of genericWebAPP'})
+    res.json({ message: 'Welcome to the Backend API of genericWebAPP', jwtSecret, apiKey})
 });
 
 app.listen(port, () => {
     console.log('Backend API is running on port ${port}');
+    console.log('JWT Secret: ${jwtSecret}');
+    console.log('API Key: ${apiKey}');
 });
